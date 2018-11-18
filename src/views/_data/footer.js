f923f5f9431e9609940fab43f839540f7f3ac71a
Reece Lucas <reece.lucas@bbc.co.uk>
2018-11-18 19:29:21 +0000

    Small styling tweaks. Footer copy added.

--- src/views/_data/footer.js
+++ src/views/_data/footer.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: 'Want to work together?',
-  text:
-    'Est voluptate in culpa pariatur sint excepteur esse adipisicing anim in est voluptate in culpa pariatur sint excepteur esse adipisicing anim in est voluptate in culpa pariatur sint excepteur esse adipisicing <a class="c-link" href="mailto:reecelucas@sky.com">reecelucas@sky.com</a>.',
+  text: `If you have a project that you'd like some help with, please don't hesitate to get in touch: <a class="c-link" href="mailto:reecelucas@sky.com">reecelucas@sky.com</a>. I'm currently available from Q2 2019 onwards.`,
   navItems: [
     {
       label: 'Github',
