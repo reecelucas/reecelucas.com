71a4c84f72b36a8c67a1273e505b0cc883999cf6
Reece Lucas <reece.lucas@bbc.co.uk>
2019-01-18 10:05:25 +0000

    Copy changes

--- src/views/_data/hero.js
+++ src/views/_data/hero.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: `
-    I'm Reece — a freelance UI Developer based in London. I work with organisations
+    I'm Reece, a freelance UI Developer based in London. I work with organisations
     big and small to build cracking digital products.
   `
 };
