ed8cf31dc07bf989257fbaaebd8ce36ec71373ec
Reece Lucas <reece.lucas@bbc.co.uk>
2019-01-20 15:38:49 +0000

    Font subsetting and work urls added

--- src/views/_data/work.js
+++ src/views/_data/work.js
@@ -5,37 +5,37 @@ module.exports = {
       img: 'assets/images/work/collective-thumbnail.png',
       title: 'Collective London',
       tags: ['UX', 'Performance', 'Accessibility'],
-      url: '/'
+      url: 'http://www.collectivelondon.com/'
     },
     {
       img: 'assets/images/work/mqa-thumbnail.png',
       title: 'MQA',
       tags: ['Symfony', 'Animation', 'UX'],
-      url: '/'
+      url: 'http://www.mqa.co.uk/'
     },
     {
       img: 'assets/images/work/markie-markerson-thumbnail.png',
       title: 'Markie Markerson',
       tags: ['React', 'UX', 'Speech API'],
-      url: '/'
+      url: 'https://github.com/reecelucas/Markie-Markerson'
     },
     {
       img: 'assets/images/work/hyundai-thumbnail.png',
       title: 'Hyundai Click to Buy',
       tags: ['ITCSS', 'CMS', 'Performance'],
-      url: '/'
+      url: 'http://clicktobuy.hyundai.co.uk/'
     },
     {
       img: 'assets/images/work/gatsby-starter-thumbnail.png',
       title: 'Gatsby Site Starter',
       tags: ['React', 'Gatsby', 'PWA'],
-      url: '/'
+      url: 'https://github.com/reecelucas/gatsby-site-skeleton'
     },
     {
       img: 'assets/images/work/lazily-thumbnail.png',
       title: 'Lazily.js',
       tags: ['Intersection Observer API', 'Performance'],
-      url: '/'
+      url: 'https://github.com/reecelucas/lazily'
     }
   ]
 };
