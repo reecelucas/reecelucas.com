1b0c43b5ef80d088a874c33556f22934986223b9
Reece Lucas <reece.lucas@bbc.co.uk>
2019-03-11 09:28:18 +0000

    Unregister previously registered SW

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,7 @@
+import unregisterServiceWorker from './modules/unregisterServiceWorker';
 import manageFocusState from './modules/manageFocusState';
 import form from './modules/form';
 
+unregisterServiceWorker();
 manageFocusState();
 form();
---
+++ src/scripts/modules/unregisterServiceWorker.js
@@ -0,0 +1,9 @@
+export default () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+  }
+};
